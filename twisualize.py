3f8c6bd8ead0410df200c1122ba573eb74169751
Benjamin Malley <benjamin.malley@gmail.com>
2012-02-01 15:28:06 -0500

    added 3rd step of oauth process

--- twisualize.py
+++ twisualize.py
@@ -1,38 +1,62 @@
 import oauth2 as oauth
 import config
 import urlparse
-from flask import Flask, redirect, url_for, session
+from flask import Flask, redirect, url_for, session, render_template
 from urllib import urlencode
+import time
 
 app = Flask(__name__)
+app.secret_key = config.CKEY
+app.consumer = oauth.Consumer(key=config.CKEY, secret=config.CSEC)
 
 @app.route('/')
 def get_auth():
 
-	consumer = oauth.Consumer(key=config.CKEY, secret=config.CSEC)
-	client = oauth.Client(consumer)
+	client = oauth.Client(app.consumer)
 
 	resp, content = client.request(config.AUTH_URL+'request_token', 'POST',
 							body=urlencode({'oauth_callback': config.SITE_URL+'/success/'}))
 	if resp['status'] != '200': raise Exception('uh oh')
 
-	session['token_request'] = dict(urlparse.parse_qsl(content))
+	session['request_token'] = dict(urlparse.parse_qsl(content))
 
-	return redirect('{0}?oauth_token={1}'.format(auth_url+'authorize',
-							session['token_request']['oauth_token']))
+	return redirect('{0}?oauth_token={1}'.format(config.AUTH_URL+'authorize',
+							session['request_token']['oauth_token']))
 
 @app.route('/success/')
 def get_token():
-	if 'token_request' in session.keys:
-		session['token'] = oauth.Token(session['token_request']['oauth_token'],
-			session['token_request']['oauth_token_secret'])
+	if 'request_token' in session.keys():
+		session['authorization_token'] = oauth.Token(session['request_token']['oauth_token'],
+			session['request_token']['oauth_token_secret'])
 		
-		client = oauth.Client(consumer, token)
+		client = oauth.Client(app.consumer, session['authorization_token'])
 		
 		resp, content = client.request(config.AUTH_URL+'access_token', 'GET')
 		if resp['status'] != '200': raise Exception('uh oh')
 
-		session['token'] = dict(urlparse.parse_qsl(content))
+		session['access_token'] = dict(urlparse.parse_qsl(content))
+		
+		request_url = 'https://api.twitter.com/1/statuses/user_timeline.json?user_id={0}&include_entities=false'.format(
+			session['access_token']['screen_name'])
+		token = oauth.Token(key=session['access_token']['oauth_token'], secret=session['access_token']['oauth_token_secret'])
+		
+		#req = oauth.Request(method='GET', url=request_url, parameters={
+		#	'oauth_version': '1.0',
+		#	'oauth_nonce': oauth.generate_nonce(),
+		#	'oauth_timestamp': int(time.time()),
+		#	'oauth_token': token.key,
+		#	'oauth_consumer_key': app.consumer.key,
+		#})
+		#req.sign_request(oauth.SignatureMethod_HMAC_SHA1(), app.consumer, token)
+		
+		client = oauth.Client(app.consumer, token)
+		
+		resp, content = client.request(request_url, 'GET')
+		print content
+		print resp
+		if resp['status'] != '200': raise Exception('uh oh')
+		
+		return 'hello world'
 
 if __name__=='__main__':
 	app.debug = config.DEBUG
