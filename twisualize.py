eb513541e5c08bdf98d20ce53c5baa724cf18f19
Benjamin Malley <benjamin.malley@gmail.com>
2012-02-05 23:59:16 -0500

    improved application flow and added json api

--- twisualize.py
+++ twisualize.py
@@ -1,7 +1,7 @@
 import oauth2 as oauth
 import config
 import urlparse
-from flask import Flask, redirect, url_for, session, render_template, request
+from flask import Flask, redirect, url_for, session, render_template, request, flash, make_response
 from urllib import urlencode
 import time
 import json
@@ -16,7 +16,10 @@ app.cache = redis.StrictRedis(host='localhost', port=6379, db=0)
 def verify_response(resp):
 	if resp['status'] != '200':
 		session.pop('request_token', None)
-		raise Exception('Bad response from Twitter: {0}'.format(resp))
+		flash('Bad response from Twitter: {0}'.format(resp))
+		return redirect(url_for('index'))
+	else:
+		return None
 
 def get_tweets(client):
 	'''Queries Twitter API for user tweets until it gets 0 back.
@@ -24,22 +27,17 @@ def get_tweets(client):
 	tweets = []
 	page = 1
 	while True: # repeat until tweet supply is exhausted 
-		url = 'https://api.twitter.com/1/statuses/user_timeline.json?count=200&page={0}'.format(page)
+		url = config.API+'1/statuses/user_timeline.json?count=200&page={0}'.format(page)
 		resp, content = client.request(url, 'GET', body=urlencode({
 			'screen_name': session['access_token']['screen_name'],
 			'include_entities': 'false',
 		}))
 		verify_response(resp)
 	
-		# TO DO: because we're just going to throw this into Redis,
-		# it doesn't make sense to parse it
-		content = json.loads(content)
-
-		if len(content) == 0: # we've run out of tweets
+		if content == '[]':
 			return tweets
-
-		page += 1 # get the next 'page' of tweets
-		tweets.extend(content)
+		page += 1 # get the next page of tweets
+		tweets.extend(json.loads(content))
 
 
 @app.route('/')
@@ -51,7 +49,6 @@ def index():
 
 	# is this an oauth callback?
 	if 'request_token' in session:
-				
 		auth_token = oauth.Token(session['request_token']['oauth_token'],
 			session['request_token']['oauth_token_secret'])
 
@@ -61,24 +58,52 @@ def index():
 
 		client = oauth.Client(app.consumer, auth_token)
 		resp, content = client.request(config.AUTH_URL+'access_token', 'GET')
+		
 		verify_response(resp)
 		
 		session['access_token'] = dict(urlparse.parse_qsl(content))
 
-		if app.cache.get(session['access_token']) == None:
+		if app.cache.get(session['access_token']['screen_name']) == None:
 			
 			token = oauth.Token(key=session['access_token']['oauth_token'],
 				secret=session['access_token']['oauth_token_secret'])
 		
 			client = oauth.Client(app.consumer, token)
 			
-			app.cache.set(session['access_token']['screen_name'],
-				json.dumps(get_tweets(client)))
+			tweets = []
+			page = 1
+			while True: # repeat until tweet supply is exhausted 
+				url = config.API+'1/statuses/user_timeline.json?count=200&page={0}'.format(page)
+				resp, content = client.request(url, 'GET', body=urlencode({
+					'screen_name': session['access_token']['screen_name'],
+					'include_entities': 'false',
+				}))
+
+				if resp['status'] != '200':
+					session.pop('request_token', None)
+					flash('Bad response from Twitter: {0}'.format(resp))
+					return redirect(url_for('index'))
+
+				if content == '[]':
+					break
+				page += 1 # get the next page of tweets
+				tweets.extend(json.loads(content))
+			
+
+			# use a pipeline so we don't accidentally leave keys in Redis
+			# that won't get cleaned up.
+			with app.cache.pipeline() as pipe:
+				pipe.set(session['access_token']['screen_name'], json.dumps(tweets))
+				# 86400 = 24 hours
+				pipe.expire(session['access_token']['screen_name'], 86400)
+				pipe.execute()
 
 		# what we should be doing here is rendering a page, processing tweets in the
 		# background and using AJAX to fill in the viz
 		# but this will do for now
-		return render_template('visualize.html')
+		resp = make_response(render_template('visualize.html'))
+		resp.set_cookie('user', session['access_token']['screen_name'])
+		return resp
 	else: # just render the standard index page
 		return render_template('index.html')
 
@@ -99,12 +124,22 @@ def authorize():
 							session['request_token']['oauth_token']))
 
 
-@app.route('/data/')
-def get_data():
+@app.route('/data/<user>/<aggregation>/')
+def get_data(user,aggregation):
 	if session['access_token'] != None:
 		data = app.cache.get(session['access_token']['screen_name'])
 		user = twitter.process_tweets(json.loads(data))
-		return str(user.tweets_by_month)
+		try:
+			return str(getattr(user, aggregation)).replace("'", '"')
+		except AttributeError:
+			# bad data aggregation request
+			return make_response('Nothing here!', 404)
+	else:
+		return make_response('You must have an valid session to complete this request.', 401)
+
+@app.route('/test/')
+def test():
+	return make_response('You must be cool!', 401)
 
 if __name__=='__main__':
 	app.debug = config.DEBUG
